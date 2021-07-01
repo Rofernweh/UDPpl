aee6bc4b3640c2549070430580aa4fb693b2dfc3
Rofernweh <rofernweh@gmail.com>
2021-07-01 12:02:51 +0100

    Add files via upload

---
+++ C++/Clase9.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//Imprimir todas las potencias positivas de 2.
+int main ()
+{	
+	for (int potencias2=1; i<100; i*=2) //Reemplaza la condicion() por el numero deseado
+	{
+		cout << i << endl;
+	}
+	
+	return 0;
+}
